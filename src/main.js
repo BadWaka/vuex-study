f0b0ee4494f13c0d4dea91ff87a71092e3c64489
wangkai37 <wangkai37@baidu.com>
2018-01-02 10:21:06 +0800

    bak

--- src/main.js
+++ src/main.js
@@ -1,15 +1,30 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// The Vue build version to load with the `import` command (runtime-only or
+// standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuex from 'vuex';
 
 Vue.config.productionTip = false
 
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    }
+  }
+});
+
+store.commit('increment');
+
+console.log('store.state.count', store.state.count);
+
 /* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
-})
+new Vue({el: '#app', router, template: '<App/>', components: {
+    App
+  }})
